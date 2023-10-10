d5a1b8b2c4daca9d57c707dd3029e8795384a5f9
juyong31 <actyoung31@naver.com>
2023-10-10 20:28:42 +0900

    비밀번호 암호화 및 check수정

--- check.py
+++ check.py
@@ -5,19 +5,14 @@ import capture_save
 
 # 정상출력여부 확인
 def check_and_go_back(driver):
-  keyword = "검색하신 자료는 존재하지 않습니다. 검색조건을 확인해 보시기 바랍니다."
-
   try:
+    keyword = "검색하신 자료는 존재하지 않습니다. 검색조건을 확인해 보시기 바랍니다."
+    wait = WebDriverWait(driver, 5)
     # 첫 번째 조건 평가
-    WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, "//*[contains(text(),'"+keyword+"')]"))
+    wait.until(
+        EC.presence_of_element_located((By.XPATH, "//*[contains(text(), '"+keyword+"')]"))
+        | EC.presence_of_element_located((By.XPATH, "/html/body/center/table[1]/tbody/tr/td[1]/span"))
     )
   except:
-    try:
-      # 두 번째 조건 평가
-      WebDriverWait(driver).until(
-          EC.presence_of_element_located((By.XPATH, "/html/body/center/table[1]/tbody/tr/td[1]/span"))
-      )
-    except:
-      capture_save.capture_and_save()
-      driver.back()
+    capture_save.capture_and_save()
+    driver.back()
--- login.py
+++ login.py
@@ -1,9 +1,24 @@
 from selenium.webdriver.common.by import By
+import os
+
+
+#비밀번호 암호화
+def password_encrypt():
+  password_file = "password.txt"
+  if os.path.exists(password_file):
+    with open(password_file, "r") as file:
+      password = file.readline().strip()
+      return password
+  else:
+    raise FileNotFoundError("Password file not found")
+  
+  
 
 #로그인
 def login_excute(driver):
   username = "actyoung31"
-  password = "^Hi69lA!" #임시비밀번호
+  
+  password = password_encrypt()
 
   username_input = driver.find_element(By.NAME, "user_id")
   password_input = driver.find_element(By.NAME, "user_pw")
