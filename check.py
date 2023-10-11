6480bc0b7f9055fe09888d59e3dbea0e23263f68
juyong31 <actyoung31@naver.com>
2023-10-11 19:51:33 +0900

    check.py 수정
    find_element or
    find_elements or
    의 차이점 확인 필요

--- check.py
+++ check.py
@@ -6,13 +6,8 @@ import capture_save
 # 정상출력여부 확인
 def check_and_go_back(driver):
   try:
-    keyword = "검색하신 자료는 존재하지 않습니다. 검색조건을 확인해 보시기 바랍니다."
     wait = WebDriverWait(driver, 5)
-    # 첫 번째 조건 평가
-    wait.until(
-        EC.presence_of_element_located((By.XPATH, "//*[contains(text(), '"+keyword+"')]"))
-        | EC.presence_of_element_located((By.XPATH, "/html/body/center/table[1]/tbody/tr/td[1]/span"))
-    )
+    wait.until(lambda driver: driver.find_elements(By.XPATH,"/html/body/center/table[2]/tbody/tr[1]/td[1]") or driver.find_elements(By.XPATH,"/html/body/center/font"))
   except:
     capture_save.capture_and_save()
     driver.back()
